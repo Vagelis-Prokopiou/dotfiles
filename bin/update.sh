b73e904721a13f7b9be3fbcb9c82e430da0102ac
Vagelis Prokopiou <drz4007@gmail.com>
2017-02-18 13:53:20 +0200

    Updating NodeJS version
    Signed-off-by: Vagelis Prokopiou <drz4007@gmail.com>

--- bin/update.sh
+++ bin/update.sh
@@ -175,7 +175,7 @@ function main-update() {
 
 	# ----- Install NodeJS. -----
 	function installNodeJS {
-		curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -;
+		curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -;
 		sudo apt-get install -y nodejs;
 	}
 
