5107aa7177d12178e136fd92f2678664d0ebcca5
Vagelis Prokopiou <drz4007@gmail.com>
2017-03-18 21:56:38 +0200

    update.sh
    Signed-off-by: Vagelis Prokopiou <drz4007@gmail.com>

--- bin/update.sh
+++ bin/update.sh
@@ -133,7 +133,12 @@ UUID=52AF7EBE182A63E2   /media/va/52AF7EBE182A63E2   ntfs    auto,user,exec,rw,s
 	############################################
 	# Sublime 3 (3126)
 	wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb && dpkg -i sublime*.deb -y;
-	# Install Monokai-Midnight as theme.
+	# Install Monokai-Midnight as theme: http://colorsublime.com/theme/Monokai-Midnight
+    wget 'http://colorsublime.com/theme/download/61775';
+    sudo mkdir -p ~/.config/sublime-text-3/Packages/Themes;
+    sudo cp 61775 /root/.config/sublime-text-3/Packages/Themes/Monokai-Midnight.tmTheme;
+    sudo cp 61775 ${user_home}/.config/sublime-text-3/Packages/Themes/Monokai-Midnight.tmTheme;
+
 
 	############################################
 	# ----- Install Sublime Text 3 package manager
