63d89cadf5ae98f8433e906fb88035d926031e38
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-10-26 22:33:18 +0300

    Initial iteration of the install-tor.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

---
+++ bin/install-tor.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd ~/Downloads;
+
+# Get the package.
+wget "https://www.torproject.org$(curl -s https://www.torproject.org/download/ | grep downloadLink | grep linux | awk '{ print $3}' | cut -d'"' -f 2)";
+
+# Unzip it.
+mkdir tor && tar -xvf tor-browser-linux*tar.xz -C tor;
+
+rm -rf tor-browser-linux*tar.xz;
+
+# Find the executable
+bash ~/Downloads/tor/start-tor-browser;
