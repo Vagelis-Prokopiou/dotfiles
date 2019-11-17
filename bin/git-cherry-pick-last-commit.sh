06b14a0a7bec4aee2571d0c5d6d86995c67a9dac
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-11-17 11:31:44 +0200

    Adding git pull to bin/git-cherry-pick-last-commit.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

---
+++ bin/git-cherry-pick-last-commit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
+
+target_branch="develop";
+
+if [[ $1 ]]; then    
+    target_branch="$1";
+fi
+
+commit=$(git log | head -n 1 | awk '{ print $2 }'); \
+current_branch=$(git status | head -n 1 | awk '{ print $3 }'); \
+git checkout "${target_branch}" \
+&& git pull \
+&& git cherry-pick "${commit}" \
+&& git push \
+&& git checkout "${current_branch}" \
+&& echo "Commit ${commit} was successfully cherry picked";
