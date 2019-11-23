d8329256fbfc06ea0709ad2f07964c171cae76ed
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-11-23 20:12:58 +0200

    Multiple updates to git-cherry-pick-last-commit.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/git-cherry-pick-last-commit.sh
+++ bin/git-cherry-pick-last-commit.sh
@@ -8,11 +8,15 @@ if [[ $1 ]]; then
     target_branch="$1";
 fi
 
-commit=$(git rev-parse HEAD); \
-current_branch=$(git status | head -n 1 | awk '{ print $3 }'); \
-git checkout "${target_branch}" \
-&& git pull \
-&& git cherry-pick "${commit}" \
-&& git push \
-&& git checkout "${current_branch}" \
-&& echo "Commit ${commit} was successfully cherry picked";
+commit=$(git rev-parse HEAD);
+commit_short=$(git rev-parse --short HEAD);
+current_branch=$(git status | head -n 1 | awk '{ print $3 }');
+
+git checkout "${target_branch}";
+git pull 2> /dev/null;
+git cherry-pick "${commit}";
+git push 2> /dev/null;
+git checkout "${current_branch}";
+
+cherry_picked_commit=$(git rev-parse --short ${target_branch});
+echo "Commit ${commit_short} was successfully cherry picked to branch ${target_branch} as commit ${cherry_picked_commit}.";
