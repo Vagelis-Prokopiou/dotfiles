97fbdbd0fae87227e2226c627f60465bca2a35a9
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-02 14:25:48 +0200

    Adding dotnet-create-systemctl-entry.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

---
+++ bin/dotnet-create-systemctl-entry.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
+
+if [ ! "$1" ]; then
+    echo "Usage: $0 <app_name>";
+    echo "Example: $0 myapp";
+    exit 1;
+fi
+
+www="/home/va/www";
+app_name="$1";
+distribution=$(lsb_release -a 2> /dev/null | grep --color=auto Description | sed 's/^Description:\s\+//');
+
+echo "[Unit]
+Description=.NET Core App running on ${distribution}
+
+[Service]
+WorkingDirectory=${www}/${app_name}/public_html
+ExecStart=/usr/bin/dotnet ${www}/${app_name}/public_html/${app_name}.dll
+Restart=always
+# Restart service after 10 seconds if the dotnet service crashes:
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=dotnet-example
+User=va
+Environment=ASPNETCORE_ENVIRONMENT=Production 
+
+[Install]
+WantedBy=multi-user.target" | sudo tee "/etc/systemd/system/kestrel-${app_name}.service";
