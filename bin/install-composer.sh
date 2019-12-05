37a818e354f6d7a2e4dc557ed3b429ad9868f676
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-05 09:27:27 +0200

    Creating install-composer.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

---
+++ bin/install-composer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
+
+# Instructions from: https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+sudo mv composer.phar /usr/local/bin/composer;
+echo "composer has been succesfully installed to /usr/local/bin";
+
+exit $RESULT
