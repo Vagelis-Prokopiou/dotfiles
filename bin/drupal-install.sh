82b0cdf7370ca63c872ca05833734453f2f5966b
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-12 21:51:52 +0200

    Creating the files dir
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/drupal-install.sh
+++ bin/drupal-install.sh
@@ -23,15 +23,17 @@ sudo rm -rf ./.*;
 
 composer create-project drupal/recommended-project . --no-interaction;
 composer install;
+
+mkdir -p web/sites/default/files;
+chmod -R 777 web/sites/default/files;
+
 rm -rf .git;
-
 sudo chown -R va:www-data .;
-
 git init;
 git add .;
 git commit -m 'Initial commit';
 
 mysql -u${user_pass} -p${user_pass} -e "DROP DATABASE IF EXISTS $1; CREATE DATABASE $1;";
 # drush si -y standard --root=./web --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/${1} --account-name ${user_pass} --account-pass ${user_pass};
-drush si -y standard --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/${1} --account-name ${user_pass} --account-pass ${user_pass};
+drush si -y standard --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/${1};
 drush cr;
