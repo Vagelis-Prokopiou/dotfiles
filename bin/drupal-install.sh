edce85d625ca9447c174f8236c42ee9633fca1cf
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-14 22:16:41 +0200

    Updating drupal-install.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/drupal-install.sh
+++ bin/drupal-install.sh
@@ -2,6 +2,13 @@
 
 # Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
 
+if [[ ! "$1" ]]; then
+    echo "Usage: $0 <database_name>";
+    exit 1;
+fi
+
+database="$1";
+
 command -v composer > /dev/null 2>&1 || install-composer.sh;
 sudo apt install -y php-mysql && sudo systemctl restart apache2;
 sudo apt install -y php-xml;
@@ -11,29 +18,31 @@ sudo apt install -y php-curl;
 sudo apt install -y php-mbstring;
 sudo apt install -y php-gd;
 
-
-if [[ ! "$1" ]]; then
-    echo "Usage: $0 <database_name>";
-    exit 1;
-fi
-
-user_pass=root;
-sudo rm -rf ./*;
-sudo rm -rf ./.*;
+user_pass="root";
+sudo rm -rf ./* > /dev/null;
+sudo rm -rf ./.* > /dev/null;
 
 composer create-project drupal/recommended-project . --no-interaction;
 composer install;
 
+mkdir ./tmp;
 mkdir -p web/sites/default/files;
 chmod -R 777 web/sites/default/files;
 
 rm -rf .git;
-sudo chown -R va:www-data .;
+touch .gitignore;
+echo '.idea' >> .gitignore;
+echo 'core' >> .gitignore;
+echo 'module/contrib/*' >> .gitignore;
+echo 'themes/contrib/*' >> .gitignore;
+echo 'sites/default/*' >> .gitignore;
+
 git init;
 git add .;
 git commit -m 'Initial commit';
 
-mysql -u${user_pass} -p${user_pass} -e "DROP DATABASE IF EXISTS $1; CREATE DATABASE $1;";
-# drush si -y standard --root=./web --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/${1} --account-name ${user_pass} --account-pass ${user_pass};
-drush si -y standard --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/${1};
+mysql -u "${user_pass}" -p"${user_pass}" -e "DROP DATABASE IF EXISTS $1; CREATE DATABASE ${database};";
+drush si -y standard --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/"${database}" --account-name="${user_pass}" --account-pass="${user_pass}";
 drush cr;
+
+sudo chown -R va:www-data .;
