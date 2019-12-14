689bc51b4591d4252f78c7761430d137b9248e3b
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-14 16:07:42 +0200

    Updates to git-delete-branch.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/git-delete-branch.sh
+++ bin/git-delete-branch.sh
@@ -2,25 +2,26 @@
 
 # Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
 
-# The "git branch -rd origin/<branchName>" removed the local branch reference, when all other failed.
-if [[ $1 ]]; then
-    git checkout master 2> /dev/null;
-    branch_name="$1";
-    
-    echo "Deleting local $branch_name branch...";
-    git branch -D "$branch_name";
-    
-    echo "Deleting remote $branch_name branch...";
-    git push origin --delete "$branch_name";
-    
-    # If the develop branch is available switch to it.
-    git checkout develop 2> /dev/null;
-	
-	# Updates all local branch references.
-    git remote prune origin;
-    
-    echo "Your current branches are:";
-    git branch -a;
-else
-    echo "Usage: git-delete-branch <branch_name>";
+if [[ ! "$1" ]]; then
+    echo "Usage: $0 <branch_name>";
+    exit 1;    
 fi
+
+branch_name="$1";
+
+git checkout master;
+
+echo "Deleting local $branch_name branch...";
+git branch -D "$branch_name";
+
+echo "Deleting remote $branch_name branch...";
+git push origin --delete "$branch_name";
+
+# Updates all local branch references.
+git remote prune origin;
+
+# If the develop branch is available switch to it.
+git checkout develop 2> /dev/null;
+
+echo "Your current branches are:";
+git branch -a;
