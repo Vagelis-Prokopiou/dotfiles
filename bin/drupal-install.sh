0b91a8db976dc4f3f693cd13aa9f0f3abda10087
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2019-12-15 11:15:59 +0200

    Updating drupal-install.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/drupal-install.sh
+++ bin/drupal-install.sh
@@ -2,14 +2,11 @@
 
 # Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
 
-if [[ ! "$1" ]]; then
-    echo "Usage: $0 <database_name>";
-    exit 1;
-fi
-
-database="$1";
-
+# Check for composer.
 command -v composer > /dev/null 2>&1 || install-composer.sh;
+
+# Check for PHP.
+# Todo: Extract PHP dependencies to a respective script.
 sudo apt install -y php-mysql && sudo systemctl restart apache2;
 sudo apt install -y php-xml;
 sudo apt install -y php-imagick;
@@ -18,31 +15,43 @@ sudo apt install -y php-curl;
 sudo apt install -y php-mbstring;
 sudo apt install -y php-gd;
 
-user_pass="root";
+# Define variables.
+db_user_pass="root";
+database=$(pwd | sed 's|.*www/||; s|/public_html||; s|\.|_|');
+
+# Clear the current directory.
 sudo rm -rf ./* > /dev/null;
 sudo rm -rf ./.* > /dev/null;
 
+# Install Drupal.
 composer create-project drupal/recommended-project . --no-interaction;
 composer install;
 
+# Make the missing directories.
 mkdir ./tmp;
 mkdir -p web/sites/default/files;
-chmod -R 777 web/sites/default/files;
+chmod -R 775 web/sites/default/files;
 
-rm -rf .git;
+# Create .gitignore
 touch .gitignore;
-echo '.idea' >> .gitignore;
-echo 'core' >> .gitignore;
-echo 'module/contrib/*' >> .gitignore;
-echo 'themes/contrib/*' >> .gitignore;
-echo 'sites/default/*' >> .gitignore;
+{ echo '.idea'; \
+  echo 'core'; \
+  echo 'module/contrib/*'; \
+  echo 'themes/contrib/*'; \
+  echo '**/*/sites/default/*'; \
+} >> .gitignore;
 
+# Update the owner.
+sudo chown -R va:www-data .;
+
+# Create a new repo.
 git init;
 git add .;
 git commit -m 'Initial commit';
 
-mysql -u "${user_pass}" -p"${user_pass}" -e "DROP DATABASE IF EXISTS $1; CREATE DATABASE ${database};";
-drush si -y standard --db-url=mysql://${user_pass}:${user_pass}@localhost:3306/"${database}" --account-name="${user_pass}" --account-pass="${user_pass}";
+# Create the database.
+mysql -u "${db_user_pass}" -p"${db_user_pass}" -e "DROP DATABASE IF EXISTS ${database}; CREATE DATABASE ${database};";
+drush si -y standard --db-url=mysql://${db_user_pass}:${db_user_pass}@localhost:3306/"${database}" --account-name="${db_user_pass}" --account-pass="${db_user_pass}";
 drush cr;
 
-sudo chown -R va:www-data .;
+
