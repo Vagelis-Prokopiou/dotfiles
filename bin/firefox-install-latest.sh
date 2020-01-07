3ccf98149ec9b2f47b4e5a071883237d8d0ad382
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2020-01-07 21:14:24 +0200

    Updating firefox-install-latest.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/firefox-install-latest.sh
+++ bin/firefox-install-latest.sh
@@ -2,23 +2,28 @@
 
 # Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
 
-command -v wget || ( echo -e "Installing wget.\n\n" ; sudo apt install wget );
-wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US";
+command -v wget || (
+  echo "Installing wget"
+  sudo apt install wget
+)
+
+archive_name=firefox.latest.tar.bz2
+
+wget -O "${archive_name}" "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 
 if [ ! -d "/opt/firefox" ]; then
-    sudo mkdir /opt/firefox;
+  sudo mkdir /opt/firefox
 else
-    sudo rm -r /opt/firefox/*;
+  sudo rm -r /opt/firefox/*
 fi
 
-tar xjf FirefoxSetup.tar.bz2 -C /opt/firefox/;
+tar xjf "${archive_name}" -C /opt/firefox/
 
 if [ ! -f "/usr/lib/firefox-esr/firefox-esr_orig" ]; then
-    mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr_orig;
+  mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr_orig
 else
-    sudo rm /usr/lib/firefox-esr/firefox-esr;
+  sudo rm /usr/lib/firefox-esr/firefox-esr
 fi
 
-sudo ln -s /opt/firefox/firefox/firefox /usr/lib/firefox-esr/firefox-esr;
-rm -rf ./FirefoxSetup.tar.bz2;
-
+sudo ln -s /opt/firefox/firefox/firefox /usr/lib/firefox-esr/firefox-esr
+rm -rf "${archive_name}"
