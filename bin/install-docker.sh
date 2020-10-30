fff3b99ffba236eace7d605dc7d770c1069b6da6
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2020-10-30 17:58:14 +0200

    Updating the install docker script
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -2,8 +2,12 @@
 
 # Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
 
+sudo apt-get remove docker docker-engine docker.io containerd runc;
+sudo apt-get update;
 sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common;
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -;
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable";
 sudo apt  update -y;
 sudo apt install -y docker-ce;
+sudo usermod -aG docker $USER;
+
