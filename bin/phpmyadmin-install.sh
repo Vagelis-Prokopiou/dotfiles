1b4513f21c241ebaa260cbf367e96f68bff39524
DadaHumpa01 <74679282+DadaHumpa01@users.noreply.github.com>
2021-03-04 10:28:57 +0100

    here is now that the script works
    apt install "afessdemamt"14

--- bin/phpmyadmin-install.sh
+++ bin/phpmyadmin-install.sh
@@ -1,74 +1,35 @@
-#!/usr/bin/env bash
+mkdir etc/nginx/ssl
+apt update
+apt upgrade -y
+apt install nginx -y
+apt install mariadb-server -y
+apt install php-mbstring php-zip php-gd php-mysql php7.3-fpm -y
 
-# Author: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/42.pem -keyout /etc/nginx/ssl/42.key -subj "/C=IT/ST=Napoli/L=Napoli/O=4 /OU=Napoleone/CN=42"
 
-latest=$(curl --silent https://www.phpmyadmin.net/ | grep href | grep files | head -n 1 | sed 's/.*href=//g; s/"//g' |  awk '{ print $1 }');
-wget $latest;
-unzip phpMyAdmin*;
-rm -rf phpMyAdmin*.zip;
-sudo mv phpMyAdmin* /usr/share/phpmyadmin;
-sudo chown -R www-data:www-data /usr/share/phpmyadmin;
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
+tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz
 
-# Create the dababase.
-sudo mysql -u root -p"root" -e "DROP DATABASE IF EXISTS phpmyadmin;";
-sudo mysql -u root -p"root" -e "CREATE DATABASE phpmyadmin DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
-sudo mysql -u root -p"root" -e "GRANT ALL ON phpmyadmin.* TO 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;";
+mv phpMyAdmin-4.9.7-all-languages/ phpmyadmin
+mv phpmyadmin /var/www/html
+rm phpMyAdmin-4.9.7-all-languages.tar.gz
 
-# Install the necessary php-extensions.
-sudo apt install -y php-imagick php-phpseclib php-php-gettext php7.3-common php7.3-gd php7.3-imap php7.3-json php7.3-curl php7.3-zip php7.3-xml php7.3-mbstring php7.3-bz2 php7.3-intl php7.3-gmp;
+service nginx start
+service mysql start
+service php7.3-fpm start
 
+echo "CREATE DATABASE wpdb;" | mysql -u root --skip-password
+echo "CREATE USER 'wpuser'@'localhost' identified by 'dbpassword';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-sudo a2disconf phpmyadmin.conf && systemctl reload apache2;
-echo "# phpMyAdmin default Apache configuration
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/html
+mv /tmp/wp-config.php /var/www/html/wordpress
+rm latest.tar.gz
 
-Alias /phpmyadmin /usr/share/phpmyadmin
+service php7.3-fpm start
+service nginx start
 
-<Directory /usr/share/phpmyadmin>
-    Options SymLinksIfOwnerMatch
-    DirectoryIndex index.php
-
-    <IfModule mod_php5.c>
-        <IfModule mod_mime.c>
-            AddType application/x-httpd-php .php
-        </IfModule>
-        <FilesMatch ".+\.php$">
-            SetHandler application/x-httpd-php
-        </FilesMatch>
-
-        php_value include_path .
-        php_admin_value upload_tmp_dir /var/lib/phpmyadmin/tmp
-        php_admin_value open_basedir /usr/share/phpmyadmin/:/etc/phpmyadmin/:/var/lib/phpmyadmin/:/usr/share/php/php-gettext/:/usr/share/php/php-php-gettext/:/usr/share/javascript/:/usr/share/php/tcpdf/:/usr/share/doc/phpmyadmin/:/usr/share/php/phpseclib/
-        php_admin_value mbstring.func_overload 0
-    </IfModule>
-    <IfModule mod_php.c>
-        <IfModule mod_mime.c>
-            AddType application/x-httpd-php .php
-        </IfModule>
-        <FilesMatch ".+\.php$">
-            SetHandler application/x-httpd-php
-        </FilesMatch>
-
-        php_value include_path .
-        php_admin_value upload_tmp_dir /var/lib/phpmyadmin/tmp
-        php_admin_value open_basedir /usr/share/phpmyadmin/:/etc/phpmyadmin/:/var/lib/phpmyadmin/:/usr/share/php/php-gettext/:/usr/share/php/php-php-gettext/:/usr/share/javascript/:/usr/share/php/tcpdf/:/usr/share/doc/phpmyadmin/:/usr/share/php/phpseclib/
-        php_admin_value mbstring.func_overload 0
-    </IfModule>
-
-</Directory>
-
-# Disallow web access to directories that don't need it
-<Directory /usr/share/phpmyadmin/templates>
-    Require all denied
-</Directory>
-<Directory /usr/share/phpmyadmin/libraries>
-    Require all denied
-</Directory>
-<Directory /usr/share/phpmyadmin/setup/lib>
-    Require all denied
-</Directory>" | sudo tee /etc/apache2/conf-available/phpmyadmin.conf;
-
-# Create temp folder.
-sudo mkdir -p /var/lib/phpmyadmin/tmp;
-sudo chown www-data:www-data /var/lib/phpmyadmin/tmp;
-
-sudo a2enconf phpmyadmin.conf && sudo systemctl reload apache2;
+bin/bash
