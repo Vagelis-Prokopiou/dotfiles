f526fe802cf2ab29c384cb3211b0026f0c44c5d7
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-04-24 17:43:45 +0300

    Adding download-segmented-file.sh
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

---
+++ bin/download-segmented-file.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+help() {
+  echo ""
+  echo "Run the script by providing the master.m3u8 url, which has the form:"
+  echo "https://s29.wolfstream.tv/hls/aoqo7chc5kr5fwfuh27bk5q3lgo456s5kkuhxkoys,siua76tccwp63ccqhwa,y6ua76tccwoqrffdzka,6vuc76tccwlxnzhynbq,.urlset/master.m3u8"
+  echo "The script uses youtube-dl under the hood."
+  echo "Example usage:"
+  echo "$0 <url>"
+  echo ""
+}
+
+# if [ ! "$2" ]; then
+if [ ! "$1" ]; then
+  help
+  exit 1
+fi
+
+# Create a temp dir
+tmpDir=/tmp/movie
+echo "Creating tmp dir: $tmpDir"
+mkdir -p $tmpDir
+cd $tmpDir
+rm -rf ./*
+
+# Set the variables.
+url="$1"
+
+# See https://stackoverflow.com/questions/22188332/download-ts-files-from-video-stream (youtube-dl)
+# Get the best of the available formats
+format=$(youtube-dl -F "${url}" | grep best | awk '{print $1}');
+
+# Download
+youtube-dl --format ${format} ${url}
+
+ls -1
+vlc ./*.mp4
