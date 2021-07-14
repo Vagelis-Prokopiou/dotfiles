6e02ebb2f99eef5b74ef7433028ae1b3befb9649
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-07-14 17:54:42 +0300

    Caches clearing updates

--- bin/update.sh
+++ bin/update.sh
@@ -235,13 +235,8 @@ function clear-caches() {
 	sudo rm -rf ${user_home}/java_error*;
 	sudo rm -rf ${user_home}/drush-backups/*;
 	sudo rm -rf /root/drush-backups/*;
-	sudo find /var/www -iname "*.gz" | grep -v "*.sql.gz" | xargs sudo rm -r;
-	sudo find /var/www/html -iname ".com.google.Chrome*" | xargs sudo rm -r;
-	sudo find /var/www/ -type f -name '*log' | while read file; do echo -n > "$file"; done;
-	sudo find /root /home /var/www "$dir" -ipath "*/.cache/*" -type f -delete;
-	# sudo find /root /home /var/www "$dir" -ipath "*/cache/*" -type f -delete;
-	sudo find /root /home /var/www "$dir" -ipath "*/tmp/*" -type f -delete;
-	sudo find /var/www/ -path "*/devel_themer/*" -delete;
+	sudo find /root /home -ipath "*/.cache/*" -type f -delete;
+	sudo find /root /home -ipath "*/tmp/*" -type f -delete;
 	sudo find /root /home -name "*.log" -delete;
 	echo "clear-caches ended"
 	echo
